b79e5212f4c734b16ce2a3065d64e36a627213cf
airellzulkarnain <airell.zulkarnain@gmail.com>
2022-04-23 20:53:26 +0700

    query fix

--- API/main.py
+++ API/main.py
@@ -38,28 +38,28 @@ cursor = db.cursor()
 
 @app.post('/insert/')
 async def insert_db(user: User):
-    cursor.execute('INSERT INTO user (name, username, password) VALUES (%s,%s, MD5(%s))', (user.name, user.username, user.password))
+    cursor.execute('INSERT INTO "user" (name, username, password) VALUES (%s,%s, MD5(%s))', (user.name, user.username, user.password))
     db.commit()
     return {'message': f'added user row with {user.name}|{user.username}', 'status':'oke'}
 
 @app.put('/update/{column}/{id}/')
 async def update_db(column:Column, id: int, value: str = Body(..., embed=True)):
     if column.value == Column.Password.value:
-        cursor.execute('UPDATE user SET password=MD5(%s) WHERE ID=%s', (value, id))
+        cursor.execute('UPDATE "user" SET password=MD5(%s) WHERE id=%s', (value, id))
     if column.value == Column.Name.value:
-        cursor.execute('UPDATE user SET name=%s WHERE ID=%s', (value, id))
+        cursor.execute('UPDATE "user" SET name=%s WHERE id=%s', (value, id))
     if column.value == Column.Username.value:
-        cursor.execute('UPDATE user SET username=%s WHERE ID=%s', (value, id))
+        cursor.execute('UPDATE "user" SET username=%s WHERE id=%s', (value, id))
     db.commit()
     return {'message': f'successfully update {column.value} of table with ID: {id} to {value}'}
 
 @app.delete('/delete/{id}/')
 async def delete_db(id: int):
-    cursor.execute('DELETE FROM user WHERE ID=%s',(id, ))
+    cursor.execute('DELETE FROM "user" WHERE id=%s',(id, ))
     db.commit()
     return {'message': f'table with ID: {id} have been deleted'}
 
 @app.get('/select/')
 async def select_db():
-    cursor.execute('SELECT ID, name, username FROM user')
+    cursor.execute('SELECT id, name, username FROM "user"')
     return {"message": "query success", "row": cursor.fetchall()}
